2370ca654119a27cfc5a1e5f58c536b92dcec00f
Marina Nazina <nzyame@gmail.com>
2024-08-03 13:24:49 +0100

    generics

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,9 +1,17 @@
+type Person = {
+	name: string;
+};
+
+type Age = {
+	age: number;
+};
+
 function merge<T, U>(objA: T, objB: U): T & U {
 	return Object.assign({}, objA, objB);
 }
 
-const objA = { name: 'Alice' };
-const objB = { age: 30 };
+const objA: Person = { name: 'Alice' };
+const objB: Age = { age: 30 };
 
 const mergedObj = merge(objA, objB);
 console.log(mergedObj);
